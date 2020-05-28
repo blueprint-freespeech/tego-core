15aacf8b7537b3f934c56d50a12cde0b33caa1b4
teor <teor@riseup.net>
2020-05-28 18:13:20 +1000

    Replace modified files with equivalents from tor
    Copy ed25519_tor.c and ed25519_donna_tor.h from
    tor/src/ext/ed25519/donna commit c13a81f7f9 to
    ricochet/lib/onion_ed25519_signature ed25519.c and sign.h.
    
    Using the following shell script:
    
    cd ~/tor/
    git checkout c13a81f7f9
    cd ~/ricochet/lib/onion_ed25519_signature
    cp ~/tor/src/ext/ed25519/donna/ed25519_tor.c ed25519.c
    cp ~/tor/src/ext/ed25519/donna/ed25519_donna_tor.h sign.h
    
    This is an analysis-only commit.

--- lib/onion_ed25519_signature/sign.h
+++ lib/onion_ed25519_signature/sign.h
@@ -1,34 +1,38 @@
-//
-// Created by haojun on 8/1/20.
-//
+/* Added for Tor. */
+#ifndef SRC_EXT_ED25519_DONNA_H_INCLUDED_
+#define SRC_EXT_ED25519_DONNA_H_INCLUDED_
+#include "lib/cc/torint.h"
 
-#ifndef ED25519_SIGN_VERIFY_SIGN_H
-#define ED25519_SIGN_VERIFY_SIGN_H
-#include "ed25519-donna.h"
-#include "ed25519-hash.h"
-#include <string.h>
-#include <stdlib.h>
-#include <assert.h>
+typedef unsigned char curved25519_key[32];
 
-typedef unsigned char ed25519_signature[64];
-typedef unsigned char ed25519_public_key[32];
-typedef unsigned char ed25519_secret_key[32];
+int ed25519_sign_open_batch_donna(const unsigned char **m, size_t *mlen, const unsigned char **pk, const unsigned char **RS, size_t num, int *valid);
+void curved25519_scalarmult_basepoint_donna(curved25519_key pk, const curved25519_key e);
 
-unsigned char * convert(char *s);
+/* Tor specific interface to match the `ref10` glue code. */
+int ed25519_donna_selftest(void);
+int ed25519_donna_seckey(unsigned char *sk);
+int ed25519_donna_seckey_expand(unsigned char *sk, const unsigned char *sk_seed);
+int ed25519_donna_pubkey(unsigned char *pk, const unsigned char *sk);
+int ed25519_donna_keygen(unsigned char *pk, unsigned char *sk);
+
+int ed25519_donna_open(const unsigned char *signature, const unsigned char *m,
+  size_t mlen, const unsigned char *pk);
 
 int ed25519_donna_sign(unsigned char *sig, const unsigned char *m, size_t mlen,
-                   const unsigned char *sk, const unsigned char *pk);
+  const unsigned char *sk, const unsigned char *pk);
 
-int ed25519_sign_open(const unsigned char *m, size_t mlen, const ed25519_public_key pk, const ed25519_signature RS);
+int ed25519_donna_blind_secret_key(unsigned char *out, const unsigned char *inp,
+  const unsigned char *param);
 
-static void
-ed25519_hram(hash_512bits hram, const ed25519_signature RS, const ed25519_public_key pk, const unsigned char *m, size_t mlen) {
-    ed25519_hash_context ctx;
-    ed25519_hash_init(&ctx);
-    ed25519_hash_update(&ctx, RS, 32);
-    ed25519_hash_update(&ctx, pk, 32);
-    ed25519_hash_update(&ctx, m, mlen);
-    ed25519_hash_final(&ctx, hram);
-}
+int ed25519_donna_blind_public_key(unsigned char *out, const unsigned char *inp,
+  const unsigned char *param);
 
-#endif //ED25519_SIGN_VERIFY_SIGN_H
+int ed25519_donna_pubkey_from_curve25519_pubkey(unsigned char *out,
+  const unsigned char *inp, int signbit);
+
+
+int
+ed25519_donna_scalarmult_with_group_order(unsigned char *out,
+                                          const unsigned char *pubkey);
+
+#endif
